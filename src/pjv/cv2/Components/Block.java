4a80f3005a66f07ccf6bfa0482f8e73a49347673
Lubomir Cuhel <cuhellub@fit.cvut.cz>
2013-10-02 13:46:09 +0200

    volume and surface methods for block

--- src/pjv/cv2/Components/Block.java
+++ src/pjv/cv2/Components/Block.java
@@ -4,10 +4,32 @@
  */
 package pjv.cv2.Components;
 
+import java.awt.Color;
+
 /**
  *
  * @author cuhellub
  */
-public class Block {
+public class Block extends Component {
+    double a, b ,c;
+
+    public Block(double a, double b, double c, Color color, Type type) {
+        super(color, type);
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    
+
+    @Override
+    public double getSurface() {
+        return 2*(a*b + b*c + a*c);       
+        
+    }
+
+    @Override
+    public double getVolume() {
+        return a*b*c;
+    }
     
 }
